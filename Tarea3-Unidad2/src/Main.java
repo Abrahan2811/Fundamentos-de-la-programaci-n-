3052db68413befb654e93ae7ed9400b55f76eac5
Memingod <abrahamvieyra05@gmail.com>
2024-09-14 14:30:05 -0600

    Tarea

---
+++ Tarea3-Unidad2/src/Main.java
@@ -0,0 +1,59 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static <Int> void main(String[] args) {
+
+        System.out.println("Mi libro favorito:") ;
+
+        String libroFav = "La republica de platon" ;
+
+        String Autor = "Platon" ;
+
+        int añoPublicacion = -400 ;
+
+        String genero = "Dialogó" ;
+
+        int numeroPaginas = 654 ;
+
+        System.out.println("Titulo: " + libroFav) ;
+        System.out.println("Autor: " + Autor) ;
+        System.out.println("Año de publicacion: " + añoPublicacion) ;
+        System.out.println("Genero: " + genero) ;
+        System.out.println("Numero de paginas: " + numeroPaginas) ;
+
+        System.out.println("        ");
+
+        //Seccion 2  Información de un país que deseas visitar
+        System.out.println("Pais que deseo visitar: ");
+
+        String pais = ("Alemania") ;
+        String Capital = ("Berlín") ;
+        String idiomaOf = ("Aleman") ;
+        int Poblacion = (83243573);
+        String Moneada = ("Euro");
+
+        System.out.println("Nombre del pais: " + pais) ;
+        System.out.println("Capital del pais: " + Capital) ;
+        System.out.println("Idioma del pais: " + idiomaOf) ;
+        System.out.println("Poblacion: " + Poblacion) ;
+        System.out.println("Moneada: " + Moneada) ;
+
+        System.out.println("      ");
+        //Sección 3: Información de un personaje histórico favorito
+
+        String NombrePersonaje = ("Aristocles Podros(Platon)");
+        int fechaNacimiento = (-427);
+        String Nacionalidad = ("Griego");
+        String Ocupacion = ("Filósofo");
+        String Logro = ("Fundó la Academia, considerada la primera institución de educación superior en Europa.");
+
+        System.out.println("Nombre del personaje: " + NombrePersonaje) ;
+        System.out.println("Fecha nacimiento: " + fechaNacimiento) ;
+        System.out.println("Nacionalidad: " + Nacionalidad) ;
+        System.out.println("Ocupacion: " + Ocupacion) ;
+        System.out.println("Logro: " + Logro) ;
+
+        System.out.println("       ");
+
+        }
+    }
