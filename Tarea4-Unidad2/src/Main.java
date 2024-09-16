0850d74a9c26350a04a530e467baba0b9ab3b3f6
Memingod <abrahamvieyra05@gmail.com>
2024-09-16 15:33:40 -0600

    Tarea4/Unidad2-Terminada

---
+++ Tarea4-Unidad2/src/Main.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //primera formula
+
+        int ResultadoformulaUno = ((12 + 8) * (5 - 3) - (6 / 2));
+
+        double ResultadoformulaunoDos = ((3 * 4) + (8 / 2));
+
+        double DivisionUno = ResultadoformulaUno / ResultadoformulaunoDos;
+
+        System.out.println(DivisionUno);
+        System.out.println("      ");
+
+        //Segunda Formula
+
+        double divisorDos = (((9 * 5) - (15 + 3) + (8 * 2) - (12 / 4)) * (7 + 2) - (6 * 3/2)) / (2 + (3 * 2)/(4 - 2)) + (5 * ((8/4) + 1) - (6 / (2 + 1)));
+        double dividendoDos = (((4 + 2) * (7 - 1)) / (3 + 1) + ((3 * 2) - (5 + 1)) * (8 / 2)) / (3 + ((6/2) + (7 / 2))) + (2 * (3 + (8 / 4)));
+        double resultadoDos = divisorDos / dividendoDos;
+        System.out.println(resultadoDos);
+        System.out.println(" ");
+
+        //tercera formula
+
+        double primeraParte = ((6 + 4) * 3) - (18 / 2);
+
+        double segundaParte = (8 - 5) + (3 * 2);
+
+        double DivisionTres = primeraParte / segundaParte;
+
+        System.out.println(DivisionTres);
+
+
+
+
+
+
+
+
+
+    }
+    }
