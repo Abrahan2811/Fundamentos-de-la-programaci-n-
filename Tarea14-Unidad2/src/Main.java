e26b154a249034cd0997ec2ab0149944684c4026
Memingod <abrahamvieyra05@gmail.com>
2024-10-04 00:48:40 -0600

    Tarea Completada

--- Tarea14-Unidad2/src/Main.java
+++ Tarea14-Unidad2/src/Main.java
@@ -3,37 +3,45 @@
 public class Main {
     public static void main(String[] args) {
 
-        String correo = "juan.Perez@empresa.com";
-        double fechadeAsceso = 2023-04-15;
-        double Salario = 50000.75;
+        String correo = "juan.perez@empresa.com";
+        String fechaAscenso = "2023-04-15";
+        double salario = 50000.75;
 
-        //nombre y apellido
-        int primerpunto = correo.indexOf(".");
-        String nombre = correo.substring(0,primerpunto);
+        //indicar de que punto partimospara el correo y la parte despues del "@"
+        int indiceCorreo = correo.indexOf('@');
+        String correoDos = correo.substring(0, indiceCorreo);
+        String segundaParte = correo.substring(indiceCorreo);
+        //funcion para inveritir el nombre
+        String correoInvertido = new StringBuilder(correoDos).reverse().toString();
+        //System.out.println(correoInvertido+segundaParte);
+
+        //indicar punto para localizar fecha
+        int primerguion = fechaAscenso.indexOf("-");
+        int ultimoguion = fechaAscenso.lastIndexOf("-");
+
+        //localizacion
+        String año = fechaAscenso.substring(0,primerguion);
+        String mes = fechaAscenso.substring(primerguion+1,ultimoguion);
+        String dia = fechaAscenso.substring(ultimoguion+1);
+        //cambiamos el String con (Integer.valueOf())
+        int suma = Integer.valueOf(año) + Integer.valueOf(mes) + Integer.valueOf(dia);
+        //System.out.println(suma);
+
+        //convertirlo
+        String conversion = Double.toString(salario);
+        int punto = conversion.indexOf(".");
+        String entero = conversion.substring(0,punto);
+        String decimal = conversion.substring(punto+1);
+       // System.out.println(entero+ " " + decimal);
+
+        double diferencia = Integer.valueOf(entero) - Integer.valueOf(decimal);
+        double diferenciaDos = Integer.valueOf(decimal) - Integer.valueOf(entero);
+        double sumaDiferencias = diferencia + diferenciaDos;
+        double multiplicacion = sumaDiferencias * 2;
+        //System.out.println(multiplicacion);
 
 
-        int arroba = correo.indexOf("@");
-        String apellido= correo.substring(primerpunto,arroba);
-
-        //invertir nombre
-
-        String nombreinvertido = nombre.replace(nombre,apellido)
-                .replace("p","/")
-                .replace("z","p")
-                .replace("/","z");
-
-        String apellidoInvertido = apellido.replace(apellido,nombre)
-                .replace("j","/")
-                .replace("u","")
-                .replace("a","u")
-                .replace("n","j")
-                .replace("/","n")
-                .replace("","a");
-
-
-        System.out.println(nombreinvertido+ apellidoInvertido);
-
-
+        System.out.println(correoInvertido+segundaParte + "|" + suma + "|" + multiplicacion);
 
     }
 }
